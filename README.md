19ccf3b6930f4d074d174ca7bd05a26293ba7993
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2023-05-01 14:06:41 +0200

    get interp working with args

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # lia-angular-test
 
 POC write Angular tests (wip)
+
+## how it works
+
+```bash
+haxe build_interp.hxml -i path/to/folder
+//
+haxe build_interp.hxml -i _testme/one
+```
--- src/Main.hx
+++ src/Main.hx
@@ -1,4 +1,16 @@
+import haxe.io.Path;
+import sys.io.Process;
+
+using StringTools;
+
 class Main {
+	/**
+	 * 0.0.1 	initial release
+	 */
+	var VERSION:String = '0.0.1';
+
+	var ROOT:String = '';
+
 	var dirCount = 0;
 	var fileCount = 0;
 
@@ -8,20 +20,24 @@ class Main {
 	var startTime:Date;
 	var endTime:Date;
 
-	public function new() {
+	public function new(?args:Array<String>) {
 		// Sys.command('clear'); // will produce a `TERM environment variable not set.`
 		info('Start project: "${Project.NAME}"');
 		// check time
 		startTime = Date.now();
 
-		initLog();
-		init();
-		// setupProject();
-		createTree();
-		recursiveLoop(Folder.SOURCE_FRONTEND);
+		initArgs(args);
 
-		// info('dirCount: $dirCount', 1);
-		// info('fileCount: $fileCount', 1);
+		if (ROOT == '')
+			return;
+
+		// initLog();
+		// init();
+		// setupProject();
+		recursiveLoop(ROOT);
+
+		info('dirCount: $dirCount', 1);
+		info('fileCount: $fileCount', 1);
 		// info('ignoreArr: $ignoreArr', 1);
 
 		// do something clever
@@ -31,16 +47,62 @@ class Main {
 		warn('Time to complete conversion: ${((endTime.getTime() - startTime.getTime()) / 1000)} sec');
 	}
 
-	function init() {
-		Folder.ROOT_FOLDER = Sys.getCwd();
-		Folder.BIN = Path.join([Sys.getCwd(), 'bin']);
-		Folder.DIST = Path.join([Sys.getCwd(), 'dist']);
-		Folder.ASSETS = Path.join([Sys.getCwd(), 'assets']);
-		// info('Folder.ROOT_FOLDER: ${Folder.ROOT_FOLDER}');
-		// info(Folder.BIN);
-		// info(Folder.DIST);
-		// info('Folder.ASSETS: ${Folder.ASSETS}');
+	function initArgs(?args:Array<String>) {
+		var args:Array<String> = args;
+
+		if (args.length == 0)
+			args.push('-h');
+
+		for (i in 0...args.length) {
+			var temp = args[i];
+			switch (temp) {
+				case '-v', '-version':
+					Sys.println('version: ' + VERSION);
+				case '-cd', '-folder': // isFolderSet = true;
+				case '-help', '-h':
+					showHelp();
+				case '-out', '-o':
+					// log(args[i + 1]);
+					writeOut();
+				case '-in', '-i':
+					log('root path: "${args[i + 1]}"');
+					ROOT = args[i + 1];
+				default:
+					// trace("case '" + temp + "': trace ('" + temp + "');");
+			}
+		}
 	}
+
+	function writeOut() {
+		var str = '# README\n\n**Generated on:** ${Date.now()}\n**Target:**';
+		writeFile(Sys.getCwd(), 'TESTME.MD', str);
+	}
+
+	/**
+	 * simply write the files
+	 * @param path 		folder to write the files (current assumption is `EXPORT`)
+	 * @param filename	(with extension) the file name
+	 * @param content	what to write to the file (in our case markdown)
+	 */
+	function writeFile(path:String, filename:String, content:String) {
+		if (!sys.FileSystem.exists(path)) {
+			sys.FileSystem.createDirectory(path);
+		}
+		// write the file
+		sys.io.File.saveContent(path + '/${filename}', content);
+		trace('written file: ${path}/${filename}');
+	}
+
+	// function init() {
+	// 	Folder.ROOT_FOLDER = Sys.getCwd();
+	// 	Folder.BIN = Path.join([Sys.getCwd(), 'bin']);
+	// 	Folder.DIST = Path.join([Sys.getCwd(), 'dist']);
+	// 	Folder.ASSETS = Path.join([Sys.getCwd(), 'assets']);
+	// 	// info('Folder.ROOT_FOLDER: ${Folder.ROOT_FOLDER}');
+	// 	// info(Folder.BIN);
+	// 	// info(Folder.DIST);
+	// 	// info('Folder.ASSETS: ${Folder.ASSETS}');
+	// }
 
 	/**
 	 * [Description]
@@ -73,15 +135,6 @@ class Main {
 	}
 
 	/**
-	 * quick and dirty export from install app called `tree`
-	 */
-	function createTree() {
-		info('Create tree of: "${Project.NAME}"');
-		Sys.command('cd', [Folder.SOURCE_FRONTEND]);
-		Sys.command('tree > ${Folder.BIN}/tree.md');
-	}
-
-	/**
 	 * test custom loggin
 	 */
 	function initLog() {
@@ -93,7 +146,20 @@ class Main {
 		progress("this is a progress message");
 	}
 
+	function showHelp():Void {
+		Sys.println('------------------------------------------------
+Lia-angular-test ($VERSION)
+
+	-version / -v   : version number
+	-help / -h      : show this help
+	-in / -i	    : path to project folder
+	-out / -o       : write readme (WIP)
+------------------------------------------------
+');
+
+	}
+
 	static public function main() {
-		var app = new Main();
+		var app = new Main(Sys.args());
 	}
 }
