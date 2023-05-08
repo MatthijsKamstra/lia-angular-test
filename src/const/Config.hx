3a6a86f81297e9e065cf769d11ea27f7466f4b20
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2023-05-08 22:48:58 +0200

    ⚙️ 0.0.6

--- src/const/Config.hx
+++ src/const/Config.hx
@@ -7,9 +7,10 @@ class Config {
 	 * 0.0.3 - POC services
 	 * 0.0.4 - services update
 	 * 0.0.5 - service typedef update, POST and GET, multiple functions, data in test needed
+	 * 0.0.6 - test based upon return types (bool, string, etc)
 	 *
 	 */
-	public static final VERSION:String = '0.0.5';
+	public static final VERSION:String = '0.0.6';
 
 	public static var PATH = ''; // file or folder
 	public static var IS_OVERWRITE = false;
