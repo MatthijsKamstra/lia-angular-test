cbcfe4ea192465d1ad73234135d9d34859fc8fb5
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2023-05-08 12:09:09 +0200

    better version

--- src/const/Config.hx
+++ src/const/Config.hx
@@ -1,7 +1,15 @@
 package const;
 
 class Config {
-	public static final VERSION:String = '0.0.1';
+	/**
+	 * 0.0.1 - initial code
+	 * 0.0.2 - setup code reading, overwrite, etc
+	 * 0.0.3 - POC services
+	 * 0.0.4 - services update
+	 *
+	 */
+	public static final VERSION:String = '0.0.3';
+
 	public static var PATH = ''; // file or folder
 	public static var IS_OVERWRITE = false;
 	public static var IS_DRYRUN = false;
