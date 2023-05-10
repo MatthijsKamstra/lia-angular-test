d64cf43e39cd100cef27424db62e4d26d25a5e2c
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2023-05-10 12:54:16 +0200

    📘

--- src/AST.hx
+++ src/AST.hx
@@ -14,15 +14,30 @@ package;
  */
 typedef FuncObj = {
 	var URL:String; // get this specific url for this function (is weird?)
+	var requestType:String; // POST|GET (weird?)
+	//
 	var access:String; // private|public|none
 	var name:String; // function-name
 	var returnValue:TypedObj;
 	var params:Array<TypedObj>;
-	var requestType:String; // POST|GET
 	var _content:String; // the original value
 }
 
-//  Observable<IConfigSettings>
+/**
+ * Convert a type to an object
+ *
+ *
+ * Examples that should work
+ *
+ * ## returnvalue
+ *
+ * - `Observable<IConfigSettings>`
+ * - `bool`
+ * - `ITerm[]`
+ *
+ * ## param type
+ *
+ */
 typedef TypedObj = {
 	@:optional var access:String; // private|public|none|null (not for return types)
 	@:optional var name:String; // not sure
