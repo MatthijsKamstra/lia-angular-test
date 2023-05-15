09e5d32293abf858621dae23f507dfc702e4e4d2
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2023-05-15 13:26:22 +0200

    return value examples

--- example-angular-project/src/app/service/returnvalue.service.ts
+++ example-angular-project/src/app/service/returnvalue.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ReturnvalueService {
 
-  constructor() { }
+	constructor() { }
+
+	returnBoolean(): boolean {
+		return true;
+	}
+
+	returnString(): string {
+		return "string";
+	}
+
+	returnArrayString(): string[] {
+		return ["string", "string"];
+	}
+
+	returnNumber(): number {
+		return 1;
+	}
+
+	returnBooleanNull(): boolean | null {
+		return true;
+	}
+
+	returnVoid(): void {
+		console.log('void');
+
+	}
+
+	returnNoVoid() {
+		console.log('no void');
+	}
 }
